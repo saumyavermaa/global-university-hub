5a1bdb722b532da6004dd53b60b90dbf72129aba
Saumya Verma <saumya.vrmaa@gmail.com>
2026-04-11 20:44:59 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,10 @@
+const fallbackData = [
+    { name: "University of Delhi", country: "India", state: "Delhi", web: "http://www.du.ac.in" },
+    { name: "IIT Bombay", country: "India", state: "Maharashtra", web: "https://www.iitb.ac.in" },
+    { name: "Harvard University", country: "United States", state: "Massachusetts", web: "https://www.harvard.edu" },
+    { name: "Stanford University", country: "United States", state: "California", web: "https://www.stanford.edu" },
+    { name: "University of Oxford", country: "United Kingdom", state: "Oxford", web: "https://www.ox.ac.uk" },
+    { name: "University of Toronto", country: "Canada", state: "Ontario", web: "https://www.utoronto.ca" },
+    { name: "University of Melbourne", country: "Australia", state: "Victoria", web: "https://www.unimelb.edu.au" },
+    { name: "Technical University of Munich", country: "Germany", state: "Bavaria", web: "https://www.tum.de" }
+];
